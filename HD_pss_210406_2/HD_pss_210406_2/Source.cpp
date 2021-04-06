fae9737db721bbc065a29f072ec500211a10d66f
DESKTOP-BHJPBEP\gravi <gravityfree01@gmail.com>
2021-04-06 22:09:28 +0900

    문2) 조장님 풀이

--- HD_pss_210406_2/HD_pss_210406_2/Source.cpp
+++ HD_pss_210406_2/HD_pss_210406_2/Source.cpp
@@ -1,12 +1,26 @@
 #include<stdio.h>
 
 void main() {
-    float PI = 3.141592;
+    /*float PI = 3.141592;
     int diameter = 10;
     float radius = diameter / 2;
     float roundOfCircle = diameter * PI;
     float areaOfCircle = radius * radius * PI;
+    */
 
-    printf("원의 넓이 = %f, 원의 둘레 = %f\n", roundOfCircle, areaOfCircle);
+    int d = 0;
+    float roun = 0;
+    float n = 0;
+
+    float PI = 3.141592
+
+    pritnf("지름값을 읿력해 주세요, 정수값을 입력하세요\n");
+    scanf_s("%d", &d);
+    fseek(stdin, 0, SEEK_END);
+
+    float r = d / 2;
+
+    printf("둘레 %.1f 넓이 %f\n", d * PI, (d / 2) * PI);
+    //printf("원의 넓이 = %f, 원의 둘레 = %f\n", roundOfCircle, areaOfCircle);
     fgetc(stdin);
 }
